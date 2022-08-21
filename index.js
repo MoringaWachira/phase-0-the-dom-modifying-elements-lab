10ab2d19a074535131ae5bc64120749d5db37a42
first-name second-name <silvia.wachira@student.moringa.com>
2022-08-21 21:43:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here!
+
+
+
+const element = document.getElementById("main")
+element.remove();
+
+// const firstChild = main.querySelector("main(1)");
+// main.removeChild(firstChild);
+
+// const element = document.getElementById(victory);
+
+// main.remove();
+
+// const main = document.getElementById("main");
+// main.innerHTML = <h1>victory</h1>
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "Silvia is the champion"
+body.append(newHeader);
